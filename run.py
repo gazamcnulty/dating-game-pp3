5e5d63dfe5b0a2aeb9f87d38ef29e4d9c4cda0d6
gazamcnulty <gazamcnulty@gmail.com>
2023-04-12 18:14:32 +0000

    import math random modules

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+import random 
+
+print(5 * 25)
+
+print(random.randrange(1, 4))
